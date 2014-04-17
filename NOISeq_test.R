3fcab00ad55c169bbc5014b2d08821765cbafe07
Ethuer <thuer.ernst@gmail.com>
2014-04-17 13:21:55 +0200

    more noiseq

--- NOISeq_test.R
+++ NOISeq_test.R
@@ -24,6 +24,17 @@ library('NOISeq')
 #ENSG00000230873          0          0          0
 
 
+# biotype table is simply a biomart output i form ' gene_name biotype '
+mybiotypes <-  read.table("~/RNAseq_project//comparisons/genebased_human_NOISeq/biotype.txt", 
+                         sep="\t", 
+                         #col.names=c("id", "name"), 
+                         header = TRUE,
+                         fill=FALSE, 
+                         strip.white=TRUE,
+                         row.names=1)
+
+head(mybiotypes)
+
 mysamples <-  read.table("~/RNAseq_project//comparisons/genebased_human_NOISeq/CBS6318_genes.txt", 
                   sep="\t", 
                   #col.names=c("id", "name"), 
@@ -36,19 +47,19 @@ head(mysamples)
 
 
 # define factors   treated/untreated   liver/ kidney    whatever the conditions are
-myfactors = data.frame(Tissue=c("untreated","treated","treated"),
-                       TissueRun = c("untreated_1","treated_1","treated_2"))
+myfactors = data.frame(Tissue=c("untreated","treated"),
+                       TissueRun = c("untreated_1","treated_1"))
 
 # combine the two information-sets to one expressiondata set
-mydata <- readData(data=mysamples, factors=myfactors)
+mydata <- readData(data=mysamples, factors=myfactors, biotype=mybiotypes,)
 mydata
 
 
 
 # normalisation via trimmed mean of M 
 myTMM = tmm(assayData(mydata)$exprs, long = 1000, lc = 0)
-myTMMdata <- readData(data=myTMM, factors=myfactors)
-mydata
+myTMMdata <- readData(data=myTMM, factors=myfactors, biotype=mybiotypes,)
+myTMMdata
 # low count filter is not neccessary
 
 #noiseq-sim needs raw normalized data
@@ -69,8 +80,12 @@ myresults_TMM <- noiseq(myTMMdata,
 #The output myresults@results[[1]]$prob gives the estimated probability of differential expression for each
 #feature.
 
-myresults@results[[1]]$prob
+head(myresults_TMM@results[[1]])
 
-DE.plot(myresults, q = 0.9, graphic = "expr", log.scale = TRUE)
-DE.plot(myresults_TMM, q = 0.9, graphic = "expr", log.scale = TRUE)
+DE.plot(myresults, q = 0.8, graphic = "expr", log.scale = TRUE)
+DE.plot(myresults_TMM, q = 0.7, graphic = "expr", log.scale = TRUE)
+
+DE.plot(myresults_TMM, chromosomes = NULL, q = 0.5, graphic = "distr")
+
+write.csv(myresults_TMM@results[[1]],file="~/RNAseq_project/comparisons/genebased_human_NOISeq/CBS6318_DE.txt")
 
